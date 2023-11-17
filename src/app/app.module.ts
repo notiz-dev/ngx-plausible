2208ead401bdcf98d9dea770f52ae8fd8459bd1d
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2023-11-17 15:23:04 +0100

    remove error handler usage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,11 @@
-import {
-  PlausibleErrorHandler,
-  PlausibleEventDirective,
-  PLAUSIBLE_ERROR_OPTIONS,
-  PlausibleErrorHandlerOptions,
-  createPlausibleErrorHandler,
-  PlausibleService,
-  createPlausibleErrorHandlerProvider,
-} from '@notiz/ngx-plausible';
-import { ErrorHandler, NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-
-const plausibleErrorOptions: PlausibleErrorHandlerOptions = {
-  logErrors: !environment.production,
-  plausibleErrorEvent: 'Error', // default event name
-};
+import { PlausibleEventDirective } from '@notiz/ngx-plausible';
 
 @NgModule({
   declarations: [AppComponent],
@@ -30,28 +16,7 @@ const plausibleErrorOptions: PlausibleErrorHandlerOptions = {
     PlausibleEventDirective,
     FormsModule,
   ],
-  providers: [
-    {
-      provide: ErrorHandler,
-      useClass: PlausibleErrorHandler,
-    },
-    {
-      provide: PLAUSIBLE_ERROR_OPTIONS,
-      useValue: plausibleErrorOptions,
-    },
-    // or use
-    // {
-    //   provide: ErrorHandler,
-    //   useFactory: (plausibleService: PlausibleService) =>
-    //     createPlausibleErrorHandler(plausibleService, {
-    //       logErrors: !environment.production,
-    //       plausibleErrorEvent: 'Error', // default event name
-    //     }),
-    //   deps: [PlausibleService],
-    // },
-    // or use
-    // createPlausibleErrorHandlerProvider({ logErrors: logErrors: !environment.production, }),
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
