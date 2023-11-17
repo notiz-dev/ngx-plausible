dcd8058322aa781f3adb79fa1c1ac32d69c44983
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2023-11-17 15:13:33 +0100

    remove error handler

--- projects/ngx-plausible/src/public-api.ts
+++ projects/ngx-plausible/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of ngx-plausible
  */
 
-export * from './lib/plausible-error-handler';
 export * from './lib/plausible-event.directive';
 export * from './lib/plausible.service';
 export * from './lib/plausible-types';
